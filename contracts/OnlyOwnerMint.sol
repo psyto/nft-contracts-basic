848d0b1e98f0b037ae60ab2c2a39ab08a61d7521
psyto <saito.hiroyuki@gmail.com>
2022-11-22 09:23:03 +0900

    access control

---
+++ contracts/OnlyOwnerMint.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.14;
+
+import "@openzeppelin/contracts@4.6.0/token/ERC721/ERC721.sol";
+
+contract OnlyOwnerMint is ERC721 {
+
+    constructor() ERC721("OnlyOwnerMint", "OWNER") {}
+
+    function nftMint(address to, uint256 tokenId) public {
+        _mint(to, tokenId);
+    }
+}
