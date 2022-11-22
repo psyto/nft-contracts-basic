2fae91bc6c21a6ef924269e94d8e10d9828d5db1
psyto <saito.hiroyuki@gmail.com>
2022-11-22 09:46:57 +0900

    update

--- contracts/OnlyOwnerMint.sol
+++ contracts/OnlyOwnerMint.sol
@@ -6,9 +6,22 @@ import "@openzeppelin/contracts@4.6.0/token/ERC721/ERC721.sol";
 
 contract OnlyOwnerMint is ERC721 {
 
-    constructor() ERC721("OnlyOwnerMint", "OWNER") {}
+    /**
+     * @dev
+     * - owner is an address, which deployed this contract.
+     */
+    address public owner;
 
-    function nftMint(address to, uint256 tokenId) public {
-        _mint(to, tokenId);
+    constructor() ERC721("OnlyOwnerMint", "OWNER") {
+        owner = _msgSender();
+    }
+
+    /**
+     * @dev
+     * - only the address, which deployed this contract, can mint.
+     */
+    function nftMint(uint256 tokenId) public {
+        require(owner == _msgSender(), "Caller is not the owner.");
+        _mint(_msgSender(), tokenId);
     }
 }
